0353fe4aa369598d0781be8fdfb18393581fcf00
Jason <changyuanjian@gmail.com>
2017-12-15 12:59:34 +0800

    update cacheManager

--- src/CacheManager.php
+++ src/CacheManager.php
@@ -2,6 +2,7 @@
 namespace Gap\Cache;
 
 use Symfony\Component\Cache\Simple\RedisCache;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
 
 class CacheManager
 {
@@ -22,32 +23,71 @@ class CacheManager
         if (!$opts = $this->optsSet[$name] ?? null) {
             throw new \Exception("cannot find config for cache [$name]");
         }
-        $adapter = $opts['adapter'] ?? 'redis';
 
-        if ('redis' === $adapter) {
-            $host = $opts['host'] ?? '127.0.0.1';
-            $port = $opts['port'] ?? 6379;
-            $database = $opts['database']??  0;
-            $client = new Client("redis://$host:$post");
-            $client->select($database);
-            $cache = new RedisCache($client);
+        if ('default' === ($opts['adapter'] ?? 'default')) {
+            $redis = $this->createRedis($opts);
+            return $redis;
+        }
 
+        if ('PSR16' === ($opts['adapter'] ?? 'default')) {
+            if('redis'===($opts['driver'] ?? 'redis')){
+                $redis = $this->createRedis($opts);
+                $cache = new RedisCache($redis);
+                
+                return $cache;
+            }
+            $className = $this->driver2Psr16Class($opts['driver']);
+            $class = new \ReflectionClass($className);
+            $cache = $class->newInstanceArgs();
+     
             return $cache;
         }
 
-        $class = adapter2Class($adapter);
-        $cache = new ReflectionClass($class);
-        
-        return $cache;
+        if ('PSR6' === ($opts['adapter'] ?? 'default')) {
+            if('redis'===($opts['driver'] ?? 'redis')){
+                $redis = $this->createRedis($opts);
+                $cache = new RedisCache($redis);
+                
+                return $cache;
+            }
+
+            $className = $this->driver2Psr6Class($opts['driver']);
+            $class = new \ReflectionClass($className);
+            $cache = $class->newInstanceArgs();
+
+            return $cache;
+        }
     }
 
-    protected function adapter2Class(string $adapter)
-    {
-        $class = 'Symfony\Component\Cache\Simple\\'.ucfirst($adapter).'Cache';
-        if(class_exists($class)){
-            return $class;
-        }
+     protected function driver2Psr16Class(string $adapter)
+     {
+         $class = 'Symfony\Component\Cache\Simple\\'.ucfirst($adapter).'Cache';
+         if(class_exists($class)){
+             return $class;
+         }
 
-        throw new \Exception("cannot find cache adapter class");
-    } 
+         throw new \Exception("cannot find cache adapter class");
+     } 
+
+     protected function driver2Psr6Class(string $adapter)
+     {
+         $class = 'Symfony\Component\Cache\Adapter\\'.ucfirst($adapter).'Cache';
+         if(class_exists($class)){
+             return $class;
+         }
+
+         throw new \Exception("cannot find cache adapter class");
+     } 
+
+     protected function createRedis($opts): \Redis
+     {
+        $host = $opts['host'] ?? '127.0.0.1';
+        $port = $opts['port'] ?? 6379;
+        $database = $opts['database'] ??  0;
+        $redis = new \Redis();
+        $redis->connect($host, $port);
+        $redis->select($database);
+
+        return $redis;
+     }
 }
